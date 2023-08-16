b3df1a1df590ad94e747a590b26fa957b9be5d06
jasong <jasong@student.42seoul.kr>
2023-08-16 23:09:27 +0900

    chore: 백업

--- backend/src/api/user/services/register-user.service.ts
+++ backend/src/api/user/services/register-user.service.ts
@@ -1,8 +1,7 @@
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { pwEncryption } from 'common/utils/encrypt';
 import { Repository } from 'typeorm';
-import { UserEntity } from '../../../common/database/entities/user.entity';
+import { UserEntity } from '../../../common/database/user/user.entity';
 import { LogisProfileDto } from '../dto/register-response.dto';
 import { RegisterRequestDto } from '../dto/user-register-request.dto';
 
@@ -15,12 +14,12 @@ export class RegisterUserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
+  //TODO: 로직 변경 및 DTO 수정
   async create(
-    //TODO: return에 user 정보 들어가도 될지 확인하기
     registerRequestDto: RegisterRequestDto,
   ): Promise<LogisProfileDto> {
     const dupUser = await this.userRepository.findOne({
-      where: { username: registerRequestDto.username },
+      where: { user: registerRequestDto.user },
     });
     if (dupUser) {
       this.logger.verbose(`dupUser : ${dupUser}`);
@@ -30,7 +29,6 @@ export class RegisterUserService {
     }
     const user: UserEntity = await this.userRepository.save({
       username: registerRequestDto.username,
-      password: pwEncryption(registerRequestDto.password),
     });
     return {
       id: user.id,
