818677b12052bb43b077fad108c9576a62ed9159
jasong <jasong@student.42seoul.kr>
2023-08-16 23:06:30 +0900

    fix: 유저 pw 로직 삭제

--- backend/src/api/auth/services/auth.services.ts
+++ backend/src/api/auth/services/auth.services.ts
@@ -1,9 +1,8 @@
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CookieManager } from 'common/auth/cookie/cookie.manager';
 import { JwtPaylaod } from 'common/auth/jwt/jwt.payload';
 import { LoginService } from 'common/auth/login/login.service';
-import { pwCompare } from 'common/utils/encrypt';
 import { Response } from 'express';
 import { LoginRequestDto } from '../dto/login-request.dto';
 
@@ -18,13 +17,6 @@ export class AuthService {
 
   async login(response: Response, loginDto: LoginRequestDto): Promise<void> {
     const user = await this.loginService.loginByName(loginDto.name);
-
-    if (!pwCompare(loginDto.password, user.password)) {
-      this.logger.verbose(
-        `${AuthService.name} login: Invalid password: ${loginDto.password}, ${user.password}`,
-      );
-      throw new UnauthorizedException('Invalid password');
-    }
 
     const jwt = this.jwtService.sign({ id: user.id } as JwtPaylaod);
 
